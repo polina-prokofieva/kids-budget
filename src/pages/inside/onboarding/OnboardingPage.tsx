2cecb294cb549cb354cdcde9a52d0554282e98db
Polina Prokofieva <polina.prokofieva@gmail.com>
2026-02-15 16:17:44 +0200

    move onboarding form to a separate component

--- src/pages/inside/onboarding/OnboardingPage.tsx
+++ src/pages/inside/onboarding/OnboardingPage.tsx
@@ -1,79 +1,10 @@
-import { useForm, type SubmitHandler } from "react-hook-form";
-
 import { LayoutInside } from "../_layout/LayoutInside";
-import type { OnboardingValues } from "./_types/form";
-import { useMemo, useState } from "react";
-import { ONBOARDING_STEPS } from "./_consts/steps";
-
-import styles from "./OnboardingPage.module.less";
-import { saveOnboarding } from "./_services/onboarding.service";
+import { OnboardingForm } from "./form/OnboardingForm";
 
 export const OnboardingPage = () => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    trigger,
-    formState: { errors },
-  } = useForm<OnboardingValues>();
-
-  const [step, setStep] = useState<number>(0);
-  const [isNextDisabled, setIsNextDisabled] = useState<boolean>(false);
-
-  const StepComponent = ONBOARDING_STEPS[step].component;
-  const currenctFieldName = ONBOARDING_STEPS[step].name;
-
-  const goToNextStep = async () => {
-    const isValid = await trigger(currenctFieldName);
-
-    if (isValid && step < ONBOARDING_STEPS.length - 1) {
-      setStep((s) => s + 1);
-      setIsNextDisabled(false);
-    }
-
-    if (!isValid)
-      setIsNextDisabled(true);
-  }
-
-  const goToPreviousStep = () => {
-    if (step > 0)
-      setStep((s) => s - 1);
-  }
-
-  const isFirst = useMemo(() => step === 0, [step]);
-  const isLast = useMemo(() => step === ONBOARDING_STEPS.length - 1, [step]);
-
-  const submit: SubmitHandler<OnboardingValues> = async (values: OnboardingValues) => {
-    saveOnboarding(values);
-  };
-
   return (
     <LayoutInside title="Onboarding">
-      <form onSubmit={handleSubmit(submit)}>
-        <StepComponent
-          name={currenctFieldName}
-          register={register}
-          watch={watch}
-          errors={errors}
-        />
-
-        <p className={styles.buttons}>
-          {!isFirst && <button
-            type="button"
-            onClick={goToPreviousStep}
-          >
-            Back
-          </button>}
-
-          {isLast ? <button type="submit">Save</button> : <button
-            type="button"
-            onClick={goToNextStep}
-            disabled={isNextDisabled}
-          >
-            Next
-          </button>}
-        </p>
-      </form>
+      <OnboardingForm />
     </LayoutInside>
   );
 }
---
+++ src/pages/inside/onboarding/form/OnboardingForm.tsx
@@ -0,0 +1,76 @@
+import { useMemo, useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+
+
+import styles from "./OnboardingForm.module.less";
+import { ONBOARDING_STEPS } from "../_consts/steps";
+import type { OnboardingValues } from "../_types/form";
+import { saveOnboarding } from "../_services/onboarding.service";
+
+export const OnboardingForm = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm<OnboardingValues>();
+
+  const [step, setStep] = useState<number>(0);
+  const [isNextDisabled, setIsNextDisabled] = useState<boolean>(false);
+
+  const StepComponent = ONBOARDING_STEPS[step].component;
+  const currenctFieldName = ONBOARDING_STEPS[step].name;
+
+  const goToNextStep = async () => {
+    const isValid = await trigger(currenctFieldName);
+
+    if (isValid && step < ONBOARDING_STEPS.length - 1) {
+      setStep((s) => s + 1);
+      setIsNextDisabled(false);
+    }
+
+    if (!isValid)
+      setIsNextDisabled(true);
+  }
+
+  const goToPreviousStep = () => {
+    if (step > 0)
+      setStep((s) => s - 1);
+  }
+
+  const isFirst = useMemo(() => step === 0, [step]);
+  const isLast = useMemo(() => step === ONBOARDING_STEPS.length - 1, [step]);
+
+  const submit: SubmitHandler<OnboardingValues> = async (values: OnboardingValues) => {
+    saveOnboarding(values);
+  };
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <StepComponent
+        name={currenctFieldName}
+        register={register}
+        watch={watch}
+        errors={errors}
+      />
+
+      <p className={styles.buttons}>
+        {!isFirst && <button
+          type="button"
+          onClick={goToPreviousStep}
+        >
+          Back
+        </button>}
+
+        {isLast ? <button type="submit">Save</button> : <button
+          type="button"
+          onClick={goToNextStep}
+          disabled={isNextDisabled}
+        >
+          Next
+        </button>}
+      </p>
+    </form>
+  );
+};
