b832b9b4a1391681272f1468b62deb39ab7222ad
Polina Prokofieva <polina.prokofieva@gmail.com>
2026-02-22 17:49:09 +0200

    remove navigation from onboarding and profile pages

--- src/pages/inside/onboarding/OnboardingPage.tsx
+++ src/pages/inside/onboarding/OnboardingPage.tsx
@@ -1,37 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Loader } from '@components/loader/Loader';
-import { auth } from '@fb/firebase';
-import { skipToken } from '@reduxjs/toolkit/query';
-import { useGetUserDocQuery } from '@store/api/user';
-
 import { LayoutInside } from '../_layout/LayoutInside';
 import OnboardingForm from './form/OnboardingForm';
 
 export default function OnboardingPage() {
-  const navigate = useNavigate();
-
-  const firebaseUser = auth.currentUser;
-
-  const {
-    data: userDoc,
-    isLoading,
-    isError,
-  } = useGetUserDocQuery(firebaseUser?.uid ?? skipToken);
-
-  useEffect(() => {
-    if (!firebaseUser) {
-      navigate('/signin');
-      return;
-    }
-
-    if (!isLoading && userDoc?.onboardingCompleted) {
-      navigate('/profile');
-    }
-  }, [firebaseUser, userDoc, isLoading, navigate]);
-
-  if (isLoading) return <Loader />;
-
   return (
     <LayoutInside title='Onboarding'>
       <OnboardingForm />
--- src/pages/inside/profile/Profile.tsx
+++ src/pages/inside/profile/Profile.tsx
@@ -1,5 +1,3 @@
-import { useNavigate } from 'react-router-dom';
-import { Loader } from '@components/loader/Loader';
 import { auth } from '@fb/firebase';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useGetUserDocQuery } from '@store/api/user';
@@ -7,26 +5,15 @@ import { useGetUserDocQuery } from '@store/api/user';
 import { LayoutInside } from '../_layout/LayoutInside';
 
 export default function Profile() {
-  const navigate = useNavigate();
-
   const firebaseUser = auth.currentUser;
 
   const {
     data: userDoc,
-    isLoading,
-    isError,
   } = useGetUserDocQuery(firebaseUser?.uid ?? skipToken);
 
   if (!firebaseUser) {
-    navigate('/signin');
-    return;
+    return null;
   }
-
-  if (!isLoading && !userDoc?.onboardingCompleted) {
-    navigate('/onboarding');
-  }
-
-  if (isLoading) return <Loader />;
 
   return (
     <LayoutInside title='Profile'>
