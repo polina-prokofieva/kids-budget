3196dfe1e4885b382acfb3867c89ebe0eca25ae5
Polina Prokofieva <polina.prokofieva@gmail.com>
2026-03-20 22:37:16 +0200

    add icon buttons

--- src/pages/inside/settings/categories/item/CategoryItem.tsx
+++ src/pages/inside/settings/categories/item/CategoryItem.tsx
@@ -1,3 +1,5 @@
+import { Pencil, Trash2 } from 'lucide-react';
+
 import type { Category } from '../_types/categories';
 import styles from './CategoryItem.module.less';
 
@@ -9,7 +11,19 @@ export const CategoryItem = ({
   return (
     <li className={styles.CategoryItem}>
       <div className={styles.name}>{name}</div>
-      <div className={styles.description}>{description}</div>
+
+      <div className={styles.description}>
+        {description}
+      </div>
+
+      <div className={styles.actions}>
+        <button className={styles.delete}>
+          <Trash2 />
+        </button>
+        <button className={styles.edit}>
+          <Pencil />
+        </button>
+      </div>
     </li>
   );
 };
